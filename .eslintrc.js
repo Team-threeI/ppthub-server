0135bf2327ae276e25c6daad33b40227ca8fd9c6
DonghyeonLee91 <donghyeonlee0908@gmail.com>
2022-10-15 21:39:45 +0900

    설정:프리티어&eslint설정
    프리티어와 eslint 설정하였습니다.

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    semi: "warn",
+    "no-unused-vars": "warn",
+  },
+  ignorePatterns: ["!.eslintrc.js", "!.prettierrc.json"],
+};
--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
-const indexRouter = require('./routes/index');
+const indexRouter = require("./routes/index");
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
