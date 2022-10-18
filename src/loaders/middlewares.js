32eac232c77b4db92e02a9b792e2b8400bbef9c1
DonghyeonLee91 <donghyeonlee0908@gmail.com>
2022-10-18 14:33:44 +0900

    Feat: parse API
    1. parse API 구현했습니다
    2. 서버로 데이터를 받을 수 있게 express-fileupload 모듈을 이용하였습니다
    3. req.files.pptx에서 pptx파일을 확인할 수 있습니다.
    4. view데이터 추출 로직이 아직 구현중이라 일단 status코드만 보내게 하였습니다

--- src/loaders/middlewares.js
+++ src/loaders/middlewares.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const fileupload = require("express-fileupload");
 
 const middlewareLoader = async (app) => {
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json({ limit: "10mb" }));
+  app.use(express.urlencoded({ extended: false, limit: "10mb" }));
+  app.use(fileupload());
 };
 
 module.exports = middlewareLoader;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.json({ result: "PPTHub" });
+router.post("/api/parse", (req, res, next) => {
+  try {
+    const ppt = req.files.pptx;
+
+    res.status(200).json();
+  } catch {
+    res.status(500).json();
+  }
 });
 
 module.exports = router;
