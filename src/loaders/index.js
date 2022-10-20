4b8e3fc6d4b81ac165c05f454c1ac802fbfd6cfe
DonghyeonLee91 <donghyeonlee0908@gmail.com>
2022-10-21 02:36:06 +0900

    Feat: 스케줄러 추가 1. 몽고DB TTL을 설정하여 데이터를 관리하고 있지만 삭제가 안될 경우를 대비해 스케줄러로 관리하게 하였습니다. 2. node-cron을 이용하여 스케줄관리 매일 00시 정각에 하루전에 저장된 데이터를 삭제하게 하였습니다.

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -2,12 +2,14 @@ const mongooseLoader = require("./mongoose");
 const middlewareLoader = require("./middlewares");
 const routerLoader = require("./routers");
 const errorHandlerLoader = require("./errorHandler");
+const dbSchedulerLoader = require("./dbScheduler");
 
 const expressLoader = async (app) => {
   await mongooseLoader();
   await middlewareLoader(app);
   await routerLoader(app);
   await errorHandlerLoader(app);
+  await dbSchedulerLoader();
 };
 
 module.exports = expressLoader;
