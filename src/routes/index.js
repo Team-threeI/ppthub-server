75a900e3f12e3b24f7fa17cb0fe8785f38761fdd
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2022-10-20 22:37:35 +0900

    Fix: populate method 사용 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -30,8 +30,12 @@ router.post("/api/ppt/save", async (req, res, next) => {
 router.post("/api/ppt/compare", async (req, res, next) => {
   try {
     const { originalPptId, comparablePptId } = req.body;
-    const originalPpt = await Ppt.findById(originalPptId).lean();
-    const comparablePpt = await Ppt.findById(comparablePptId).lean();
+    const originalPpt = await Ppt.findById(originalPptId)
+      .populate("slides")
+      .lean();
+    const comparablePpt = await Ppt.findById(comparablePptId)
+      .populate("slides")
+      .lean();
 
     if (!originalPpt || !comparablePpt) {
       next(createError(500));
