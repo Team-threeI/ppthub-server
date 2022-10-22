52635a129426994c11699f4c8e72cbdc2bbdb21b
DonghyeonLee91 <donghyeonlee0908@gmail.com>
2022-10-22 14:49:58 +0900

    Feat: 머지로직추가
    1. 2개의 ppt파일과 클라이언트에서 머지요청리스트가 들어오면 하나의 ppt파일로 병합해주는 로직입니다
    2. 서버에서 ppt파일을 꺼냈을 때 컬렉션이 나누다보니 원래 파싱된 데이터와 차이가 있어 원래 파싱한 데이터로 변환을 거친 뒤
    머지를 진행합니다
    3. 파싱 데이터를 기준으로 pptx파일 변환 로직이 구현되어있어 머지 역시 그에 맞춰서 구현하였습니다

---
+++ src/utils/merge.js
@@ -0,0 +1,111 @@
+const getOriginalSlideItems = (originalItems, mergedData) => {
+  const itemsMap = new Map(Object.entries(mergedData.items));
+  const mergedItems = originalItems.items.filter((item) => {
+    if (
+      (itemsMap.get(item.id).diff === "modified" &&
+        !itemsMap.get(item.id).isChecked) ||
+      (itemsMap.get(item.id).diff === "deleted" &&
+        itemsMap.get(item.id).isChecked)
+    ) {
+      return item;
+    }
+
+    return null;
+  });
+
+  return mergedItems;
+};
+
+const getComparableSlideItems = (comparableItems, mergedData) => {
+  const itemsMap = new Map(Object.entries(mergedData.items));
+  const mergedItems = comparableItems.items.filter((item) => {
+    if (
+      (itemsMap.get(item.id).diff === "modified" &&
+        itemsMap.get(item.id).isChecked) ||
+      (itemsMap.get(item.id).diff === "added" &&
+        itemsMap.get(item.id).isChecked)
+    ) {
+      return item;
+    }
+
+    return null;
+  });
+
+  return mergedItems;
+};
+
+const getMergedModifiedSlides = (
+  modifiedOriginalSlides,
+  modifiedComparableSlides,
+) => {
+  return modifiedOriginalSlides.map((slide) => {
+    const tempSlide = slide;
+    const MatchedSlide = modifiedComparableSlides.find(
+      (comparableSlide) => slide.slideId === comparableSlide.slideId,
+    );
+    if (MatchedSlide) {
+      tempSlide.items = [...slide.items, ...MatchedSlide.items];
+      return tempSlide;
+    }
+
+    return slide;
+  });
+};
+
+const getMergedPpt = (originalPpt, comparablePpt, mergeData) => {
+  const mergeDataMap = new Map(Object.entries(mergeData));
+  const mergedPpt = {
+    slideWidth: originalPpt.slideWidth,
+    slideHeight: originalPpt.slideHeight,
+  };
+
+  const modifiedOriginalSlides = originalPpt.slides
+    .filter(
+      (slide) => mergeDataMap.get(`${slide.data.slideId}`).diff === "modified",
+    )
+    .map((slide) => {
+      const { slideId, items } = slide.data;
+      return { slideId, items };
+    });
+  const modifiedComparableSlides = comparablePpt.slides
+    .filter(
+      (slide) => mergeDataMap.get(`${slide.data.slideId}`).diff === "modified",
+    )
+    .map((slide) => {
+      const { slideId, items } = slide.data;
+      return { slideId, items };
+    });
+  const addedSlides = [...originalPpt.slides, ...comparablePpt.slides]
+    .filter((slide) => mergeDataMap.get(`${slide.data.slideId}`).isChecked)
+    .map((slide) => {
+      const { slideId, items } = slide.data;
+      return { slideId, items };
+    });
+
+  for (let i = 0; i < modifiedOriginalSlides.length; i += 1) {
+    const items = getOriginalSlideItems(
+      modifiedOriginalSlides[i],
+      mergeData[modifiedOriginalSlides[i].slideId],
+    );
+    modifiedOriginalSlides[i].items = items;
+  }
+  for (let i = 0; i < modifiedComparableSlides.length; i += 1) {
+    const items = getComparableSlideItems(
+      modifiedComparableSlides[i],
+      mergeData[modifiedComparableSlides[i].slideId],
+    );
+    modifiedComparableSlides[i].items = items;
+  }
+
+  const MergedModifiedSlides = getMergedModifiedSlides(
+    modifiedOriginalSlides,
+    modifiedComparableSlides,
+  );
+
+  const slides = [...MergedModifiedSlides, ...addedSlides];
+  mergedPpt.slides = slides;
+
+  return mergedPpt;
+};
+
+module.exports = getMergedPpt;
