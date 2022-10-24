0edbf5c77081bb52307181f2b1b0e3216cece4d6
Seon <eagle0472@gmail.com>
2022-10-25 00:34:59 +0900

    Fix: CreatePpt 파싱 데이터에 맞게 수정

--- src/utils/createPpt.js
+++ src/utils/createPpt.js
@@ -1,54 +1,59 @@
 const PPTXGEN = require("pptxgenjs");
 
 const createPpt = async (mergeData) => {
-  const ppt = new PPTXGEN();
-  ppt.layout = "LAYOUT_WIDE";
+  try {
+    const ppt = new PPTXGEN();
+    ppt.layout = "LAYOUT_WIDE";
 
-  for (let i = 0; i < mergeData.slides.length; i += 1) {
-    const slide = ppt.addSlide();
+    for (let i = 0; i < mergeData.slides.length; i += 1) {
+      const slide = ppt.addSlide();
 
-    for (let y = 0; y < mergeData.slides[i].items.length; y += 1) {
-      const slideBase = mergeData.slides[i].items[y];
-      const slideContent = mergeData.slides[i].items[y].content;
+      for (let y = 0; y < mergeData.slides[i].items.length; y += 1) {
+        const slideBase = mergeData.slides[i].items[y];
+        const slideContent = mergeData.slides[i].items[y].content;
 
-      const slideContentPosition = {
-        x: slideBase.x / 96,
-        y: slideBase.y / 96,
-        w: slideBase.width / 96,
-        h: slideBase.height / 96,
-      };
+        const slideContentPosition = {
+          x: slideBase.x / 96,
+          y: slideBase.y / 96,
+          w: slideBase.width / 96,
+          h: slideBase.height / 96,
+        };
 
-      const slideContentStyle = {
-        bold: slideContent.isBold,
-        italic: slideContent.isItalic,
-        underline: slideContent.isUnderlined,
-        fontface: slideContent.font,
-        color: slideContent.color,
-        fontSize: slideContent.size,
-      };
+        const slideContentStyle = {
+          bold: slideContent.isBold,
+          italic: slideContent.isItalic,
+          underline: slideContent.isUnderlined,
+          fontface: slideContent.fontColor,
+          color: slideContent.fontcolor,
+          fontSize: slideContent.size,
+        };
 
-      if (slideBase.type === "image") {
-        slide.addImage({
-          path: slideContent.src,
-          ...slideContentPosition,
-        });
-      }
+        if (slideBase.type === "image") {
+          slide.addImage({
+            data: slideContent.src,
+            ...slideContentPosition,
+          });
+        }
 
-      if (slideBase.type === "text") {
-        slide.addText(slideContent.value, {
-          ...(slideContent.backgroundColor && {
-            fill: {
-              color: slideContent.backgroundColor,
-            },
-          }),
-          ...slideContentPosition,
-          ...slideContentStyle,
-        });
+        if (slideBase.type === "text") {
+          slide.addText(slideContent.value, {
+            ...(slideContent.backgroundColor && {
+              fill: {
+                color: slideContent.backgroundColor,
+              },
+            }),
+            ...slideContentPosition,
+            ...slideContentStyle,
+          });
+        }
       }
     }
+    const pptFile = await ppt.write("base64");
+
+    return pptFile;
+  } catch (error) {
+    return new Error(error);
   }
-  const pptFile = await ppt.write("base64");
-  return pptFile;
 };
 
 module.exports = createPpt;
