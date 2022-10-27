1309aa5a018bd2657fd7cee1df444ad4b5be52aa
Seon <eagle0472@gmail.com>
2022-10-27 22:25:56 +0900

    Fix: Test case 구체화

--- test/__mocks__/mock.js
+++ test/__mocks__/mock.js
@@ -1,11 +1,11 @@
-const saveMockData = {
+const savedMockData = {
   fileName: "MockPpt",
   pptData: {
     slideWidth: 1280,
     slideHeight: 720,
     slides: [
       {
-        slide1: "Slide 5",
+        slide1: "Slide 1",
         items: [
           {
             type: "text",
@@ -16,14 +16,14 @@ const saveMockData = {
             x: 784.5517060367455,
             y: 332.13795275590553,
             content: {
-              value: "변경이 일어나는 text item 2",
+              value: "변경이 하는 text item 2",
               isBold: true,
               isItalic: true,
               isUnderlined: false,
               fontColor: "#000000",
               backgroundColor: null,
-              align: "left",
-              size: 18,
+              align: "right",
+              size: 24,
             },
           },
           {
@@ -51,26 +51,111 @@ const saveMockData = {
   },
 };
 
-const originalPptId = "635a5f415f2ae61901638adb";
+const savedMockData2 = {
+  fileName: "MockPpt2",
+  pptData: {
+    slideWidth: 1280,
+    slideHeight: 720,
+    slides: [
+      {
+        slide1: "Slide 2",
+        items: [
+          {
+            type: "text",
+            order: 1,
+            itemId: "Text 0",
+            width: 11.5244094488189,
+            height: 31.77501312335958,
+            x: 82.5517060367455,
+            y: 12.13795275590553,
+            content: {
+              value: "변경이 일어나는 text item 3",
+              isBold: true,
+              isItalic: false,
+              isUnderlined: true,
+              fontColor: "#2b2b2b",
+              backgroundColor: null,
+              align: "left",
+              size: 18,
+            },
+          },
+          {
+            type: "text",
+            order: 3,
+            itemId: "Text 5",
+            width: 477.8479790026247,
+            height: 38.77501312335958,
+            x: 328.82761154855643,
+            y: 195.3103412073491,
+            content: {
+              value: "변경이 일어나는 text item 1 (변경되는 버전)",
+              isBold: true,
+              isItalic: true,
+              isUnderlined: false,
+              fontColor: "#000000",
+              backgroundColor: null,
+              align: "left",
+              size: 18,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
 
-const comparablePptId = "635a5e00e03c75d80b8d6234";
+const originalPptId = "635a7e23c2f144aa083a3f91";
 
-const slideOrderList = ["Slide 2", "Slide 5"];
+const comparablePptId = "635a7e23c2f144aa083a3f97";
+
+const slideOrderList = ["Slide 1", "Slide 2"];
 
 const mergeData = {
   undefined: {
-    items: { 그림이: [Object], "Text 1": [Object], "Text 0": [Object] },
+    items: {
+      "Text 5": { diff: "added", isChecked: false },
+      "Text 1": { diff: "deleted", isChecked: false },
+      "Text 0": { diff: "modified", isChecked: false },
+    },
     diff: "modified",
   },
 };
 
-const mergedPptId = "635a6ce786c28d514f6c160c";
+const mergedPptId = "635a817dd1fb0f7943cba6a6";
+
+const mergedPpt = {
+  _id: "635a817dd1fb0f7943cba6a6",
+  fileName: "MockPpt10/27/2222:02",
+  slideWidth: 1280,
+  slideHeight: 720,
+  downloadUrl:
+    "https://pptx-project-1.s3.ap-northeast-2.amazonaws.com/MockPpt10/27/2222%3A02.pptx",
+  slideOrderList: ["Slide 1", "Slide 2"],
+  slides: [],
+  createdAt: "2022-10-27T13:02:53.057Z",
+  __v: 0,
+};
+
+const failureId = "1234567891011";
+
+const failureData = {
+  undefined: {
+    items: {
+      "text 3": { diff: "deleted", isChecked: false },
+    },
+    diff: "modified",
+  },
+};
 
 module.exports = {
-  saveMockData,
+  savedMockData,
+  savedMockData2,
   slideOrderList,
   originalPptId,
   comparablePptId,
   mergeData,
   mergedPptId,
+  mergedPpt,
+  failureId,
+  failureData,
 };
--- test/integration/super.init.spec.js
+++ test/integration/super.init.spec.js
@@ -2,14 +2,18 @@ require("dotenv").config();
 
 const request = require("supertest");
 const mongoose = require("mongoose");
+
 const app = require("../../app");
 const {
-  saveMockData,
+  savedMockData,
   slideOrderList,
   originalPptId,
   comparablePptId,
   mergeData,
   mergedPptId,
+  mergedPpt,
+  failureId,
+  failureData,
 } = require("../__mocks__/mock");
 
 let agent;
@@ -26,26 +30,58 @@ afterAll(async () => {
 });
 
 it("Post /ppts/save 요청을 확인합니다.", async () => {
-  await agent.post("/ppts/save").send(saveMockData).expect(200);
+  await agent.post("/ppts/save").send(savedMockData).expect(200);
 });
 
-it("Post /ppts/compare 요청을 확인합니다.", async () => {
-  await agent
-    .post("/ppts/compare")
-    .send({ originalPptId, comparablePptId })
-    .expect(200);
+describe("Post /ppts/compare 요청을 확인합니다.", () => {
+  it("Post /ppts/compare 요청중 comparablePptId가 정상적으로 들어왔을 경우를 체크합니다.", async () => {
+    const checkdData = await agent
+      .post("/ppts/compare")
+      .send({ originalPptId, comparablePptId });
+
+    expect(JSON.parse(checkdData.text)).toEqual(mergeData);
+  });
+
+  it("Post /ppts/compare   originalPptId가 정상적으로 들어오지 않을 경우 오류를 출력합니다.", async () => {
+    await agent
+      .post("/ppts/compare")
+      .send({ failureId, comparablePptId })
+      .expect(500);
+  });
+
+  it("Post /ppts/compare 요청 중 comparablePptId가 정상적으로 들어오지 않을 경우 오류를 출력합니다.", async () => {
+    await agent
+      .post("/ppts/compare")
+      .send({ originalPptId, failureId })
+      .expect(500);
+  });
 });
 
-it("Post /ppts/merge 요청을 확인합니다.", async () => {
-  await agent
-    .post("/ppts/merge")
-    .send({ originalPptId, comparablePptId, mergeData, slideOrderList })
-    .expect(200);
+describe("Post /ppts/merge 요청을 확인합니다.", () => {
+  it("Post /ppts/merge 요청이 정상적으로 들어온 경우를 확인합니다..", async () => {
+    await agent
+      .post("/ppts/merge")
+      .send({ originalPptId, comparablePptId, mergeData, slideOrderList })
+      .expect(200);
+  });
+
+  it("Post /ppts/merge 요청 중 합칠 데이터가 비상정일 경우 오류를 출력합니다.", async () => {
+    await agent
+      .post("/ppts/merge")
+      .send({ originalPptId, comparablePptId, failureData, slideOrderList })
+      .expect(500);
+  });
 });
 
-it("Get /:ppt_id/download 요청을 확인합니다", async () => {
-  await agent
-    .get(`/${mergedPptId}/download`)
-    .query({ mergedPptId })
-    .expect(200);
+describe("Get /:ppt_id/download 요청을 확인합니다", () => {
+  it("Get /:ppt_id/download 요청이 정상적으로 들어온 경우를 확인하비다.", async () => {
+    const checkedData = await agent
+      .get(`/${mergedPptId}/download`)
+      .query({ mergedPptId });
+    expect(JSON.parse(checkedData.text)).toEqual(mergedPpt);
+  });
+
+  it("Get /:ppt_id/download 요청 중 ppt_id가 없을 경우 오류를 출력합니다.", async () => {
+    await agent.get(`/${failureId}/download`).query({ failureId }).expect(500);
+  });
 });
